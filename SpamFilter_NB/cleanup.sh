9e53e36fc17f13b3db4d06a68d7d7084ed4322b0
Tclack88 <39845330+Tclack88@users.noreply.github.com>
2019-07-11 20:23:15 -0700

    update cleanup.sh
    Now it takes the folder containing the files as an argv[1], and no longer needs to be moved into the folder to be cleaned

--- SpamFilter_NB/cleanup.sh
+++ SpamFilter_NB/cleanup.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 # used to remove unreadable bytes, the code is sensitive to non UTF-8 chars.
-# 1st line removes all characters except asci, tabs, carriage returns 
-# 2nd line removes the old files (i.e. without the prepended "cleaned.")
-# 3rd line renames the old files back to their original names
-for f in *.txt; do $(tr -cd '\11\12\15\40-\176' < $f > cleaned.$f); done
-find . -type f ! -name "clean*" -exec rm {} \;
-for f in cleaned*.txt; do mv $f ${f##cleaned.}; done
+# It removes all characters except asci, tabs, carriage returns creating a 2nd file with ".clean" appended
+# The contents of the "file.clean" is moved back to replace the original "file" then the "file.clean" is removed
+usage()
+{
+        echo "Usage: $0 path/to/dir"
+        exit 1
+}
+
+if [ $# -ne 1 ] ; then
+        usage
+else
+for f in $1/*.txt; 
+do $(tr -dc '\11\12\15\40-\176' < $f > $f.clean)
+        mv $f.clean $f
+        rm $f.clean -f
+done
+fi
