c5048559c396b836896673bc1d3498671795e458
tclack88 <tclack@aol.com>
2026-03-12 22:08:03 -0700

    add a function in encode.py to remove duplicate notes generated at the same time, so if |F|F|F|S|F is generated, it will just become |F|S for example

--- GenerateMusic_NeuralNetwork_Incomplete/steps_combined/utils/encode.py
+++ GenerateMusic_NeuralNetwork_Incomplete/steps_combined/utils/encode.py
@@ -4,8 +4,8 @@ import re
 import sys
 
 def sortstring(string):
-    string = ''.join(sorted(string))
-    return string
+	string = ''.join(sorted(string))
+	return string
 
 infile = "step1.out"
 file = open(infile,'r')
@@ -15,19 +15,19 @@ words = text.split(' ')
 
 blank = ' ' # start with a single space, otherwise erros are incurred without leading space
 for j in words:
-    if '|' in j:
-        k= j.index('|')
-        blank += ' '*int(j[0:k-1])
-        blank += '|'+j[-1]
-    elif '!' in j:
-        k = j.index('!')
-        blank += ' '*int(j[0:k-1])
-        blank += '!'+j[-1]
-    elif ' ' in j:
-        j += ' '
-    #elif "'" in j: # keep tempo information
-    #    print("tempo:",j)
-    #    blank += j
+	if '|' in j:
+		k= j.index('|')
+		blank += ' '*int(j[0:k-1])
+		blank += '|'+j[-1]
+	elif '!' in j:
+		k = j.index('!')
+		blank += ' '*int(j[0:k-1])
+		blank += '!'+j[-1]
+	elif ' ' in j:
+		j += ' '
+	#elif "'" in j: # keep tempo information
+	#	 print("tempo:",j)
+	#	 blank += j
 
 
 tempwords = ''
@@ -36,41 +36,52 @@ words = list(filter(None,words))
 # combine note_on's (|) and note off's (!)
 k = 1
 for word in words:
-    if ('!' and "|" in word):
-        while ("!" and "|" in word):
-            j, k = 0, 1
-            if word[j] == "|":
-                tempword = word[j:k]
-                while "!" not in tempword and k <= len(word):
-                    k += 1
-                    tempword = word[j:k]
-                k -= 1
-                tempword = word[j:k]
-                tempwords += tempword+' '
-                word = word[k:]
-            elif word[j] == "!":
-                tempword = word[j:k]
-                while "|" not in tempword and k <= len(word):
-                    k += 1
-                    tempword = word[j:k]
-                k -= 1
-                tempword = word[j:k]
-                tempwords += tempword+' '
-                word = word[k:]
-    else:
-        tempwords += word  # will append blank or only "note_ons" and "note_offs"
+	if ('!' and "|" in word):
+		while ("!" and "|" in word):
+			j, k = 0, 1
+			if word[j] == "|":
+				tempword = word[j:k]
+				while "!" not in tempword and k <= len(word):
+					k += 1
+					tempword = word[j:k]
+				k -= 1
+				tempword = word[j:k]
+				tempwords += tempword+' '
+				word = word[k:]
+			elif word[j] == "!":
+				tempword = word[j:k]
+				while "|" not in tempword and k <= len(word):
+					k += 1
+					tempword = word[j:k]
+				k -= 1
+				tempword = word[j:k]
+				tempwords += tempword+' '
+				word = word[k:]
+	else:
+		tempwords += word  # will append blank or only "note_ons" and "note_offs"
 blank = ''
 # findall instead of split because split returning empty entries
 words = re.split(r'(\S+)',tempwords)
 words = list(filter(None,words))
+
+def remove_duplicates(word):
+	# eg conver !N!S!N!N!T to !N!S!T (remove duplicate N's)
+	if word.isspace(): # keep blank spaces
+		return word
+	start = word[0]
+	word = start+start.join(set(word.replace(start,"")))
+	return word
+
+words = [remove_duplicates(word) for word in words]
+
 for word in words:
-    if ' ' in word:
-        blank += word
-    else:
-        char = word[0]
-        word = word.replace(char,'')
-        word = char+sortstring(word)
-        blank += word+' ' #adds a space between these notes which were together, which is ok, it's a very short time period, also adds more time to the gap before.... oh well
+	if ' ' in word:
+		blank += word
+	else:
+		char = word[0]
+		word = word.replace(char,'')
+		word = char+sortstring(word)
+		blank += word+' ' #adds a space between these notes which were together, which is ok, it's a very short time period, also adds more time to the gap before.... oh well
 
 
 
